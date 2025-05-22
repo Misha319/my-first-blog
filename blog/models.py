60932e0eca19f3b82bec24b928dc47e276751f3e
Your Name <you@example.com>
2025-05-22 11:36:23 +0300

    Added view and template for detailed blog post as well as CSS for the site.

--- blog/models.py
+++ blog/models.py
@@ -1,7 +1,7 @@
 from django.conf import settings
 from django.db import models
 from django.utils import timezone
-
+from django.shortcuts import render
 
 class Post(models.Model):
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
